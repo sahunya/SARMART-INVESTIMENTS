f183adb4f5051e7be90951bc8d914ef1c90bcdd9
sahunya <sammwaww@gmail.com>
2026-07-21 11:02:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sarmart-v92';
+const CACHE='sarmart-v94';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./sarmart-logo.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
