258104046877e50b8884cf0789496f7bfcd149ac
sahunya <sammwaww@gmail.com>
2026-08-05 14:00:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sarmart-v162';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./sarmart-logo.svg','./icon-192.png','./icon-512.png'];
+const CACHE='sarmart-v164';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase-config.js','./manifest.webmanifest','./sarmart-logo.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
